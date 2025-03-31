1b2db1655b7f8e3c43540312cb9708311f80fc63
Lê Chí Hoàng <157201498+lechihoang@users.noreply.github.com>
2025-03-31 15:13:48 +0700

    Update index.js

--- Rock paper scissors/index.js
+++ Rock paper scissors/index.js
@@ -10,7 +10,6 @@ let computerScore = 0;
 
 function playGame(playerChoice){
     const computerChoice = choices[Math.floor(Math.random() * 3)];
-    console.log(computerChoice);
     let result = "";
     if (playerChoice == computerChoice){
         result = "IT'S A TIE";
